ec5dc3221916dbcc14718f0154245e83bfb121fd
ngochoang0711 <ngochoang0711@gmail.com>
2014-04-02 00:27:16 +0700

    Viet hoa

--- What2Eat/src/com/fas/what2eat/MainActivity.java
+++ What2Eat/src/com/fas/what2eat/MainActivity.java
@@ -2,7 +2,6 @@ package com.fas.what2eat;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import com.fas.lib.MyFileLib;
@@ -10,10 +9,8 @@ import com.fas.lib.MyLogger;
 
 import android.os.Bundle;
 import android.annotation.SuppressLint;
-import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.pm.LabeledIntent;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,25 +20,22 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class MainActivity extends Activity{
 
-	//Danh sach cac mon
+	/* Initializing variables:
+	 * dishList 	: List of dishes
+	 * sa			: Array Adapter
+	 * r			: Random variable used for picking up a dish in the list
+	 * randomDialog : Dialog which is shown when user taps on the Random button
+	 * */	 
 	List<String> dishList = new ArrayList<String>();
 	ArrayAdapter<String> sa;
 	Random r = new Random();
-	
-    LinearLayout layoutOfPopup;
-    PopupWindow popupMessage;
-    Button popupButton, insidePopupButton;
-    TextView popupText;
     Dialog randomDialog;
 	
     public static final String ALL_DISHES_FILENAME = "AllDishes.txt";
@@ -49,59 +43,62 @@ public class MainActivity extends Activity{
 	public MyFileLib myIO;
 	public MyLogger myLogger;
 	
+	/*	onCreate method of the MainActivity
+	 * 
+	 * 		Create new File for saving and loading
+	 * 		Load listview from the layout
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		MyLogger myLogger = new MyLogger();
-    	String allDishesFilePath = getFilesDir() + "/" + ALL_DISHES_FILENAME;
-		ALL_DISHES_FILE = new File(allDishesFilePath);		
-		myIO = new MyFileLib();
-		myIO.createFile(ALL_DISHES_FILE);
+		String allDishesFilePath = getFilesDir() + "/" + ALL_DISHES_FILENAME;
+			ALL_DISHES_FILE = new File(allDishesFilePath);		
+			myIO = new MyFileLib();
+			myIO.createFile(ALL_DISHES_FILE);
 		
-		//Them dishList vao listview - Nhat
 		initDishes();
 		
 		ListView lv = (ListView) findViewById(R.id.listview);
-		sa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,dishList);
-		lv.setAdapter(sa);
-		registerForContextMenu(lv);
+			sa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,dishList);
+			lv.setAdapter(sa);
+			registerForContextMenu(lv);
 		
-		// Edit 1 mon trong list = cach click len mon do
+		/* EDIT FUNCTION
+		 * 		Purpose	: to rename the pre-assigned dish
+		 * 		Usage	: tap on edit button		 
+		 */
 		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-
-				
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 			    final Dialog d = new Dialog(arg0.getContext());			    
 			    TextView clickedView = (TextView) arg1;		
 			    final String mon = clickedView.getText().toString();				
 				d.setContentView(R.layout.dialog);
-				d.setTitle("Edit Dish");
+				String edit_title = "Sửa đổi thông tin món ";
+				d.setTitle(edit_title);
 				d.setCancelable(true);
 				final int pos = arg2; 
 				final EditText et = (EditText) d.findViewById(R.id.editText);
 				et.append(mon);
 				Button b = (Button) d.findViewById(R.id.btThem);
-				b.setOnClickListener(new View.OnClickListener()
-					{
-
-						@Override
-						public void onClick(View v) {
-							
-								String monEdit = et.getText().toString();
-								editDish(d,pos,monEdit);												
-						}							
+				b.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {							
+							String monEdit = et.getText().toString();
+							editDish(d,pos,monEdit);												
+					}							
 					});
 				d.show();
 			}
-		});
-		
+		});		
 		init();
 	}
 
+	/* MENU INFLATER
+	 * Purpose	: Inflate option menu when tapping on the Menu button on the Menu bar
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -117,24 +114,23 @@ public class MainActivity extends Activity{
 	      AdapterContextMenuInfo aInfo = (AdapterContextMenuInfo) menuInfo;
 	       
 	      String s = dishList.get(aInfo.position);
-	   
-	      menu.setHeaderTitle("Options for " + s);
-	      menu.add(1, 1, 1, "Delete");
+	      String menu_HeaderTitle = "Tùy chọn của ";
+	      String menu_xoa = "Xóa";
+	      menu.setHeaderTitle(menu_HeaderTitle + s);
+	      menu.add(1, 1, 1, menu_xoa);
 	       
-	  }
+	}
 
 	@Override
-	public boolean onContextItemSelected(MenuItem item)
-	{
+	public boolean onContextItemSelected(MenuItem item) {
 		deleteDish(item);
 		return true;
 	}
 	
 	/* Purpose: Xoa 1 mon trong danh sach 
-	 * PIC: Huynh Van Lam
+	 * PIC: LamHV
 	 */
 	private void deleteDish(MenuItem item){
-//		int itemId = item.getItemId();
 		AdapterContextMenuInfo aInfo = (AdapterContextMenuInfo) item.getMenuInfo();
 		dishList.remove(aInfo.position);
 		sa.notifyDataSetChanged();
@@ -142,7 +138,7 @@ public class MainActivity extends Activity{
 	}
 	
 	/* Purpose: Sua 1 mon trong danh sach
-	 * PIC: Huynh Van Lam
+	 * PIC: LamHV
 	 */
 	private void editDish(Dialog d, int pos, String monEdit)
 	{
@@ -166,13 +162,14 @@ public class MainActivity extends Activity{
 	}
 	
 	/* Purpose: Su kien khi click vao button them mon an - hien thi dialog them
-	 * PIC: Huynh Van Lam
+	 * PIC: LamHV
 	 */
 	public void addDish(View view)
 	{
 		final Dialog d = new Dialog(this);
 		d.setContentView(R.layout.dialog);
-		d.setTitle("Add Dish");
+		String Add_title = "Bổ sung món ăn";
+		d.setTitle(Add_title);
 		d.setCancelable(true);
 		Button b = (Button) d.findViewById(R.id.btThem);
 		b.setOnClickListener(new View.OnClickListener()
@@ -191,7 +188,8 @@ public class MainActivity extends Activity{
 	public void addDish(Dialog d){
 		final EditText et = (EditText) d.findViewById(R.id.editText);
 		String mon = et.getText().toString();
-		if(!checkEmptyString(mon, "Please don't enter empty value"))
+		String checkEmptyNoti = "Bạn không nên nhập rỗng.";
+		if(!checkEmptyString(mon, checkEmptyNoti))
 		{
 			MainActivity.this.dishList.add(mon);
 			MainActivity.this.sa.notifyDataSetChanged();
@@ -201,47 +199,52 @@ public class MainActivity extends Activity{
 															
 	}
 
-
+	/* INITIALIZING THE RANDOM DIALOG
+	 * 	Purpose	: To prepare for the random dialog before user tapping on the random button
+	 * 
+	 */
 	public void init() {
 		this.randomDialog = new Dialog(this);		
 		randomDialog.setContentView(R.layout.randomdialog);
-		randomDialog.setTitle("Hom nay an...");
+		String randomDialog_title = "Hãy cùng thưởng thức";
+		randomDialog.setTitle(randomDialog_title);
 		randomDialog.setCancelable(true);
-		final TextView et = (TextView) randomDialog.findViewById(R.id.chosenDish);
+		final EditText et = (EditText) randomDialog.findViewById(R.id.chosenDish);
 		et.setEnabled(false);
 		
 		Button buttonRandomAgain = (Button) randomDialog.findViewById(R.id.buttonRandomAgain);
-		buttonRandomAgain.setOnClickListener(new View.OnClickListener()
-			{
-
-				@Override
-				public void onClick(View v) {
-					int randomDishId = r.nextInt(dishList.size());
-					et.setText(dishList.get(randomDishId));
-				}							
+		buttonRandomAgain.setOnClickListener(new View.OnClickListener()	{
+			@Override
+			public void onClick(View v) {
+				int randomDishId = r.nextInt(dishList.size());
+				et.setText(dishList.get(randomDishId));
+			}							
 			});
-    }
- 
+	}
+	
+	/* SHOW RANDOM DIALOG
+	 * 	Purpose	: Pick one dish randomly and show it on the dialog
+	 */
     public void showPopup() {
-    	int listSize = dishList.size();
-    	if(listSize>0)
-    	{
-    		int randomDishId = r.nextInt(dishList.size());
-        	TextView et = (TextView) randomDialog.findViewById(R.id.chosenDish);
-        	et.setText(dishList.get(randomDishId));
-        	randomDialog.show();
-    	}
-    	
+    	EditText et = (EditText) randomDialog.findViewById(R.id.chosenDish);
+    	int randomDishId = r.nextInt(dishList.size());
+		et.setText(dishList.get(randomDishId));
+    	randomDialog.show();
     }
+    
+    /* RANDOM DISH
+     *  Purpose : Implement the show Pop method
+     * 
+     */
+    public void randomDish(View v){
+		showPopup();
+    }
+    
 	
     public void saveFile(){
 		ArrayList<String> arrayList = new ArrayList<String>(dishList); 
 		myIO.writeObjectToFile(arrayList, ALL_DISHES_FILE);
-    }
-    
-    public void randomDish(View v){
-		showPopup();
-    }
+    }    
     
     @SuppressLint("NewApi")
 	private Boolean checkEmptyString(String s, String alert){
